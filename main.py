d0884f7d04a16b459e67700dca708c95eec159b0
Alexa <wozniakola76@gmail.com>
2026-08-29 01:24:25 +0200

    Added view students and view sessions functionalities

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from PySide6.QtGui import QIcon, QPixmap, QFontDatabase, QFont
+from PySide6.scripts.project_lib import Singleton
+
 from databaseoperator import DataOperator
 from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QVBoxLayout, QPushButton, QHBoxLayout, \
-    QTableWidget, QStackedWidget, QLineEdit, QComboBox, QFrame, QDateTimeEdit
+    QTableWidget, QTableWidgetItem, QStackedWidget, QLineEdit, QComboBox, QFrame, QDateTimeEdit, QHeaderView, QAbstractItemView
 from PySide6.QtCore import Qt, QDateTime, Signal
 
 db = DataOperator()
@@ -82,14 +84,46 @@ class YourStudentsWidget(QWidget):
         card_layout.setContentsMargins(25, 20, 25, 20)
         card_layout.setSpacing(10)
 
-        heading = QLabel("LISTA TWOICH UCZNIOW")
+        heading = QLabel("LISTA TWOICH UCZNIÓW")
         heading.setObjectName("heading")
         heading.setAlignment(Qt.AlignCenter)
         card_layout.addWidget(heading)
 
+        self.table = QTableWidget()
+
+        self.table.setColumnCount(5)
+        self.table.setHorizontalHeaderLabels(["ID", "Imię", "Nazwisko", "Klasa", "Rodzaj szkoły"])
+        self.table.verticalHeader().setVisible(False)
+        header = self.table.horizontalHeader()
+        # Przypasowane do rozmiaru
+        header.setSectionResizeMode(0, QHeaderView.ResizeToContents)
+
+        # Rozwalone
+        for num in range(1,5):
+            header.setSectionResizeMode(num, QHeaderView.Stretch)
+
+        self.table.setFocusPolicy(Qt.NoFocus)
+        self.table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+
+        card_layout.addWidget(self.table)
+        self.callOutStudents()
+
         layout.addWidget(card)
 
+    def callOutStudents(self):
+        values = db.return_students()
+        print("Pobrani uczniowie:", values)
+
+        self.table.setRowCount(0)
+
+        for row_pos, row_data in enumerate(values):
+            self.table.insertRow(row_pos)
+            for col_pos, col_data in enumerate(row_data):
+                self.table.setItem(row_pos, col_pos, QTableWidgetItem(str(col_data)))
+
+
 class AddStudentWidget(QWidget):
+    seesignal = Signal()
     def __init__(self):
         super().__init__()
         layout = QVBoxLayout(self)
@@ -116,7 +150,14 @@ class AddStudentWidget(QWidget):
         card_layout.addWidget(QLabel("Klasa:"))
         self.classInput = QLineEdit()
         card_layout.addWidget(self.classInput)
-        card_layout.addWidget(QLabel("Aby móc w pełni korzystać ze statystyk uczniów klasę należy zakodować jako numer oraz literę (P/L/T)."))
+
+        card_layout.addWidget(QLabel("Rodzaj szkoły:"))
+        self.typeInput = QComboBox()
+        self.typeInput.addItems(
+            ["Liceum", "Technikum", "Branżowa I stopnia", "Branżowa II stopnia", "Podstawówka"])
+        card_layout.addWidget(self.typeInput)
+
+        card_layout.setSpacing(20)
 
         self.confirmButton = QPushButton("Zatwierdź")
         self.confirmButton.setCursor(Qt.PointingHandCursor)
@@ -129,12 +170,62 @@ class AddStudentWidget(QWidget):
         name = self.nameInput.text()
         lastname = self.lastnameInput.text()
         sclass = self.classInput.text()
+        stype = self.typeInput.currentText()
 
-        db.add_student(name, lastname, sclass)
+        db.add_student(name, lastname, sclass, stype)
+        self.seesignal.emit()
 
         self.nameInput.clear()
         self.lastnameInput.clear()
         self.classInput.clear()
+
+class ViewTutoring(QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout(self)
+        layout.setContentsMargins(20, 0, 20, 0)
+
+        card = QFrame()
+        card_layout = QVBoxLayout(card)
+        card_layout.setContentsMargins(25, 20, 25, 20)
+        card_layout.setSpacing(10)
+
+        heading = QLabel("LISTA TWOICH ZAJĘĆ")
+        heading.setObjectName("heading")
+        heading.setAlignment(Qt.AlignCenter)
+        card_layout.addWidget(heading)
+
+        self.table = QTableWidget()
+
+        self.table.setColumnCount(6)
+        self.table.setHorizontalHeaderLabels(["ID","Data zajęć","Stawka" ,"Typ zajęć" ,"Imię", "Nazwisko"])
+        self.table.verticalHeader().setVisible(False)
+        header = self.table.horizontalHeader()
+        # Przypasowane do rozmiaru
+        header.setSectionResizeMode(0, QHeaderView.ResizeToContents)
+        # Rozwalone
+        for num in range(1, 7):
+            header.setSectionResizeMode(num, QHeaderView.Stretch)
+
+        header.setSectionResizeMode(3, QHeaderView.ResizeToContents)
+
+        self.table.setFocusPolicy(Qt.NoFocus)
+        self.table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+
+        card_layout.addWidget(self.table)
+        self.callOutTutoring()
+
+        layout.addWidget(card)
+
+    def callOutTutoring(self):
+        values = db.showZajecia()
+
+        self.table.setRowCount(0)
+
+        for row_pos, row_data in enumerate(values):
+            self.table.insertRow(row_pos)
+            for col_pos, col_data in enumerate(row_data):
+                self.table.setItem(row_pos, col_pos, QTableWidgetItem(str(col_data)))
 
 
 #MAIN WINDOW
@@ -143,7 +234,7 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowIcon(QIcon('gui/images/icon'))
         self.setWindowTitle("Tutoring Management Application")
-        self.setFixedHeight(550)
+        self.setFixedSize(1280, 640)
 
         container = QWidget()
         container.setObjectName("CentralWidget")
@@ -180,14 +271,17 @@ class MainWindow(QMainWindow):
         button1 = QPushButton(' Wyświetl swoich uczniów')
         button2 = QPushButton(' Dodaj nowego ucznia')
         button3 = QPushButton(' Dodaj zajęcia')
+        button4 = QPushButton(' Wyświetl swoje zajęcia')
 
         #NO 1 PANEL
         navigationLayout.addWidget(label1)
         navigationLayout.addWidget(label3)
         navigationLayout.addWidget(self.label2)
-        navigationLayout.addWidget(button3)
-        navigationLayout.addWidget(button2)
         navigationLayout.addWidget(button1)
+        navigationLayout.addWidget(button2)
+        navigationLayout.addWidget(button3)
+        navigationLayout.addWidget(button4)
+
 
 
         #NO 2 PANEL
@@ -197,17 +291,21 @@ class MainWindow(QMainWindow):
         pagestudents = YourStudentsWidget()
         pageadding = AddStudentWidget()
         pageaddtutoring = AddTutoringWidget()
+        pageviewtutoring = ViewTutoring()
         pageaddtutoring.addedsignal.connect(self.updateEarnings)
-
+        pageadding.seesignal.connect(pagestudents.callOutStudents)
+        pageaddtutoring.addedsignal.connect(pageviewtutoring.callOutTutoring)
         #DODAWANIE
         workspace.addWidget(pagestudents)
         workspace.addWidget(pageadding)
         workspace.addWidget(pageaddtutoring)
+        workspace.addWidget(pageviewtutoring)
 
         #CONNECTION Z GUZIKAMI
         button1.clicked.connect(lambda:workspace.setCurrentIndex(0))
         button2.clicked.connect(lambda:workspace.setCurrentIndex(1))
         button3.clicked.connect(lambda: workspace.setCurrentIndex(2))
+        button4.clicked.connect(lambda: workspace.setCurrentIndex(3))
 
         #FINAL LAYOUT
         layout.addWidget(inner_container, stretch=1)
@@ -216,7 +314,7 @@ class MainWindow(QMainWindow):
     def updateEarnings(self):
         result = db.overall_zarobki()
         total = result
-        self.label2.setText(f"Twoje całkowite zarobki wynoszą <b>{total:.1f} zł.</b>")
+        self.label2.setText(f"Twoje całkowite zarobki wynoszą <b>{total:.2f} zł.</b>")
 
 
 
